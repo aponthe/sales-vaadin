23a9a8abc575737a9c16b3776543f5399f6dafd6
aponthe <46538170+aponthe@users.noreply.github.com>
2024-04-08 16:56:22 -0600

    15

--- src/main/java/com/example/demo/Layouts/RootLayout.java
+++ src/main/java/com/example/demo/Layouts/RootLayout.java
@@ -6,7 +6,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 public class RootLayout extends VerticalLayout {
     /**
      * this is the base layout for views
-     * v14
+     * v15
      */
     public RootLayout() {
         Navbar navbar = new Navbar();
